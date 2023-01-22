9192b6790583b56ea69570080e68dec7f2ad646a
Alexander Matz <alex@amatz.de>
2023-01-22 13:33:54 +0100

    align indirect threading implementation with jonesforth

--- forth.lua
+++ forth.lua
@@ -5,14 +5,13 @@
     assembly-based vm.
 
     The primary difference from jonesforth are:
+    - Implemented in lua, not assembly
     - Support for arbitrary types on the DSTACK + MEM. This is currently
       used for strings and function flags
-    - Direct threaded code instead of indirect threaded code
 
     Lua supports proper tail calls by default. This is very convenient
-    because it allows to implement the vm running (direct/indirect)
-    threaded code right away. The current implementation uses
-    direct threaded code, as MEM[NEXT_INST] is expected to contain
+    because it allows a straight forward implmenetation of the VM running
+    indirect threaded code.
 --]]
 
 --------------------------------------
@@ -30,6 +29,7 @@ LATEST = nil
 DSTACK = {}
 RSTACK = {}
 NEXT_INST = nil
+CODE_WORD = nil
 STATE = STATE_IMMEDIATE
 
 --------------------------------------
@@ -39,38 +39,47 @@ local function log(msg, ...)
 end
 
 local _cfa
-local _docol
 
 -- word header: link, name, flags, fn1, fn2, ...
 local function _create(name, flags)
-    local OFFSET = #MEM+1
+    local offset = #MEM+1
     if LATEST ~= nil then
-        MEM[LATEST] = OFFSET
+        MEM[LATEST] = offset
     end
-    table.insert(MEM, false) -- false instead of nil for link to avoid sparse arrays
+    table.insert(MEM, false) -- false instead of nil for link field to avoid sparse arrays
     table.insert(MEM, name)
     table.insert(MEM, flags)
-    return OFFSET
+    return offset
 end
 
-local function _add_word(name, flags, code)
-    assert(type(code) == "table")
-    local OFFSET = _create(name, flags)
-    table.insert(MEM, _docol(#MEM+1))
-    for _, fn in ipairs(code) do
-        table.insert(MEM, fn)
+local function _format_data(data)
+    if data == nil then
+        return "nil"
     end
-    table.insert(MEM, EXIT)
-    return _cfa(OFFSET)
+    local parts = {}
+    for idx, val in ipairs(data) do
+        if type(val) == "function" then
+            table.insert(parts, "{fn}")
+        else
+            table.insert(parts, tostring(val))
+        end
+    end
+    return "[" .. table.concat(parts, ", ") .. "]"
 end
 
-local function _add_native(name, flags, code)
-    assert(type(code) == "table")
-    local OFFSET = _create(name, flags)
-    for _, fn in ipairs(code) do
-        table.insert(MEM, fn)
+local function _add_word(name, flags, code, data)
+    assert(type(code) == "function")
+    assert(data == nil or type(data) == "table")
+    local offset = _create(name, flags)
+    local cfa = _cfa(offset)
+    table.insert(MEM, code)
+    if data then
+        for _, value in ipairs(data) do
+            table.insert(MEM, value)
+        end
     end
-    return _cfa(OFFSET)
+    -- log("adding word %s at %d, #data: %d", name, cfa, #(data or {}))
+    return cfa
 end
 
 local function _find(name)
@@ -83,7 +92,7 @@ local function _find(name)
     end
 end
 
-local function _popds ()
+local function _popds()
     if #DSTACK == 0 then
         error("trying to access empty data stack")
     end
@@ -94,42 +103,43 @@ _cfa = function (offset)
     return offset + 3
 end
 
+local function _start_vm(code_word)
+    NEXT_INST = nil
+    CODE_WORD = code_word
+    MEM[CODE_WORD]()
+end
+
+-- Interpreter for indirect threaded code
 local function _next()
-    local target = NEXT_INST
+    CODE_WORD = MEM[NEXT_INST]
     NEXT_INST = NEXT_INST + 1
-    return MEM[target]()
+    return MEM[CODE_WORD]()
 end
 
--- Embedding the next location here is required
--- because this VM currently runs direct threaded
--- code and a function is not aware
--- "where it was called from".
-_docol = function (location)
-    local function DOCOL()
-        table.insert(RSTACK, NEXT_INST)
-        NEXT_INST = location + 1
-        return _next()
-    end
-    return DOCOL
+function _DOCOL()
+    table.insert(RSTACK, NEXT_INST)
+    NEXT_INST = CODE_WORD + 1
+    return _next()
 end
+DOCOL = _DOCOL
 
-function EXIT()
+function _EXIT()
     NEXT_INST = table.remove(RSTACK)
     if NEXT_INST ~= nil then
         return _next()
     end
 end
-_add_native("EXIT", {}, {EXIT})
+EXIT = _add_word("EXIT", {}, _EXIT)
 
-function LIT()
+function _LIT()
     local val = MEM[NEXT_INST]
     table.insert(DSTACK, val)
     NEXT_INST = NEXT_INST + 1
     return _next()
 end
-_add_native("LIT", {}, {LIT})
+LIT = _add_word("LIT", {}, _LIT)
 
-function WORD()
+function _WORD()
     while true do
         if not STDIN_BUFFER then
             io.write(">>> ")
@@ -147,24 +157,23 @@ function WORD()
         end
     end
 end
-_add_native("WORD", {}, {WORD})
+WORD = _add_word("WORD", {}, _WORD)
 
-function DUMP()
+function _DUMP()
     for idx = #DSTACK,1,-1 do
         print(string.format("%d: %s", idx, tostring(DSTACK[idx])))
     end
     return _next()
 end
-_add_native("DUMP", {}, {DUMP})
+DUMP = _add_word("DUMP", {}, _DUMP)
 
-function DOT()
+function _DOT()
     print(string.format("%d", _popds()))
     return _next()
 end
-_add_native(".", {}, {DOT})
+DOT = _add_word(".", {}, _DOT)
 
-MYSUB = _add_word("MYSUB", {}, {LIT, 1337, DOT})
-MYPROGRAM = _add_word("MYPROGRAM", {}, {WORD, LIT, 2, LIT, 3, MEM[MYSUB], LIT, 4, DUMP})
+MYSUB = _add_word("MYSUB", {}, DOCOL, {LIT, 1337, DOT, EXIT})
+MYPROGRAM = _add_word("MYPROGRAM", {}, DOCOL, {WORD, LIT, 2, LIT, 3, MYSUB, LIT, 4, DUMP, EXIT})
 
-NEXT_INST = nil
-MEM[MYPROGRAM]()
+_start_vm(MYPROGRAM)
